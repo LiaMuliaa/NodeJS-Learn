77548735a62ac7e6858c8d31d686e04acee22b2c
MuliaSahpira <muliasahpira@gmail.com>
2025-10-28 09:30:40 +0700

    module system and core module

---
+++ 7 Node Core Module/app.js
@@ -0,0 +1,53 @@
+// Core Module
+// https://nodejs.org/docs/latest-v18.x/api/index.html
+// File System
+const fs = require('fs');
+
+//              menuliskan string ke file (synchronous)
+// try {
+//     fs.writeFileSync('test.txt', 'Hello World secara synchronous!');
+// } catch (e) {
+//     console.log(e);
+// }
+
+
+//              menuliskan string ke file (asynchronous)
+// fs.writeFile('test.txt', 'Hello World secara Asynchronous', (e) => {
+//     console.log(e);
+// });
+
+
+//              membaca isi file (synchronous)
+// const data = fs.readFileSync('test.txt', 'utf-8');           // utf-8 untuk ubah ke string
+// console.log(data);
+
+//              membaca isi file (asynchronous)
+// fs.readFile('test.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+
+
+//              Readline
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout, 
+});
+
+rl.question('Masukkan nama anda : ', (nama) => {
+    rl.question('Masukkan no HP anda : ', (noHP) => {
+        const contact = { nama, noHP };
+        const file = fs.readFileSync('contacts.json', 'utf-8');
+        const contacts = JSON.parse(file);
+
+        contacts.push(contact);
+
+        fs.writeFileSync('contacts.json', JSON.stringify(contacts));
+
+        console.log('Terimakasih sudah memasukkan data.');
+
+        rl.close();
+    });
+})
